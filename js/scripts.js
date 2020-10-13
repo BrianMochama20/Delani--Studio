7c4e25df60592e07b0b27d2e94eef8ad1f6f2852
BrianMochama20 <Brianobiri20@gmail.com>
2020-10-13 16:11:02 +0300

    fix error on toggle scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
 $(document).ready(function(){
-    $("#design-image").click(function(){
-        $("#design-image").slideToggle().hide();
-        $("#design h2").slideToggle();
-        $("#design p").slideToggle();
+    $("#design-image").on("click",function(){
+        $("#design-image").toggle();
+        //$("#design-h2").slideToggle();//
+        $("#design-p").slideToggle();
     });
 
-    $("#design").click(function(){
-        $("#design h2").slideToggle().hide();
-        $("#design p").slideToggle().hide();
+    $("#design").on(function(){
+        $("#design-h2").slideToggle().hide();
+        $("#design-p").slideToggle().hide();
         $("#design-image").slideToggle();
     });
 
     $("#development-image").click(function(){
         $("#development-image").slideToggle().hide();
-        $("#development h2").slideToggle();
-        $("#development p").slideToggle();
+        $("#development-h2").slideToggle();
+        $("#development-p").slideToggle();
     });
 
     $("#development").click(function(){
-        $("#development h2").slideToggle().hide();
-        $("#development p").slideToggle().hide();
-        $("#development-image").slideToggle();
+        $("#development-image").slideToggle().hide();
+        $("#development-h2").slideToggle().hide();
+        $("#development-p").slideToggle().hide();
+        
     });
 
     $("#product-image").click(function(){
         $("#product-image").slideToggle().hide();
-        $("#product h2").slideToggle();
-        $("#product p").slideToggle();
+        $("#product-h2").slideToggle();
+        $("#product-p").slideToggle();
     });
 
     $("#product").click(function(){
-        $("#product h2").slideToggle().hide();
-        $("#product p").slideToggle().hide();
+        $("#product-h2").slideToggle().hide();
+        $("#product-p").slideToggle().hide();
         $("#product-image").slideToggle();
     });
 
